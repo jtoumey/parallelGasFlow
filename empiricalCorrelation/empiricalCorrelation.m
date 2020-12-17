1a8408e6e706c3020a33ab7246adee02c18c30d1
Julian Toumey <jtoumey@gmail.com>
2020-12-17 17:41:25 -0500

    Fix small bug--need to multiply kappa by density.

--- empiricalCorrelation/empiricalCorrelation.m
+++ empiricalCorrelation/empiricalCorrelation.m
@@ -1,8 +1,12 @@
-
+% Correlation for HTCs
+clear all
+close all
+clc
+%% Global properties
 xLength = 0.2;
 
 %% topAir: Fluid region 0
-mu1 = 58.31e-6; 
+mu1 = 5.831e-5; 
 rho1 = 0.1963;
 Ux1 = 0.7432;
 
@@ -11,7 +15,7 @@ ReX1 = (rho1*Ux1*xLength)/mu1;
 Pr1 = 0.7;
 
 % Empirical
-htc1 = 0.332*ReX1^0.5*Pr1^(1./3.); 
+htc1 = (k1*rho1/xLength/2.)*0.332*ReX1^0.5*Pr1^(1./3.); 
 
 %% bottomAir: Fluid region 1
 mu2 = 1.845e-5;
@@ -22,14 +26,26 @@ k2 = 0.026515286;
 ReX2 = (rho2*Ux2*xLength)/mu2;
 Pr2 = 0.7;
 
-htc2 = 0.332*ReX2^0.5*Pr2^(1./3.); 
+htc2 =(k2*rho2/xLength/2.)*0.332*ReX2^0.5*Pr2^(1./3.); 
 
-%% 
-time = [3.33 6.5]
-TCenter1 = [808.124 ] 
-TCenter2 = [798.931 798.99]
-  
-lhs = htc1*(1800 - 810.66589)
-rhs = htc2*(300 - 798.88963)
+%% Load simulation data
+topAirT0 = 1800;
+topAirT = dlmread('./dataFiles/pgf03/postProcessing/pp0/topAir/4.91/T', '', 3);
+% topAirT = dlmread('./dataFiles/pgf04/postProcessing/pp0/topAir/7.19/T', '', 3);
+
+bottomAirT0 = 300;
+bottomAirT = dlmread('./dataFiles/pgf03/postProcessing/pp1/bottomAir/4.91/T', '', 3);
+% bottomAirT = dlmread('./dataFiles/pgf04/postProcessing/pp1/bottomAir/7.19/T', '', 3);
+
+
+%% Compute the correlation equations
+eq1 = htc1*(topAirT0 - topAirT(:, 2))
+eq2 = htc2*(bottomAirT(:, 2) - bottomAirT0)
+
+plot(topAirT(:,1), eq1, 'linewidth', 2);
+hold on;
+% plot(bottomAirT(:,1), eq2, 'linewidth', 2);
+% legend('Top Air', 'Bottom Air');
+
 
 
