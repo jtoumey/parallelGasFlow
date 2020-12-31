8e627f682d89b23f39fb92521a872da705b7b963
Julian Toumey <jtoumey@gmail.com>
2020-12-31 13:27:10 -0500

    Using latest time. Also--needed to calculate Re at the mid-point of the flat plate to be equivalent to the correlation.

--- empiricalCorrelation/empiricalCorrelation.m
+++ empiricalCorrelation/empiricalCorrelation.m
@@ -11,7 +11,7 @@ rho1 = 0.1963;
 Ux1 = 0.7432;
 
 k1 = 0.1028755;
-ReX1 = (rho1*Ux1*xLength)/mu1;
+ReX1 = (rho1*Ux1*xLength/2.)/mu1;
 Pr1 = 0.7;
 
 % Empirical
@@ -23,18 +23,18 @@ rho2 = 1.177;
 Ux2 = 0.0392;
 
 k2 = 0.026515286;
-ReX2 = (rho2*Ux2*xLength)/mu2;
+ReX2 = (rho2*Ux2*xLength/2.)/mu2;
 Pr2 = 0.7;
 
 htc2 =(k2*rho2/xLength/2.)*0.332*ReX2^0.5*Pr2^(1./3.); 
 
 %% Load simulation data
 topAirT0 = 1800;
-topAirT = dlmread('./dataFiles/pgf03/postProcessing/pp0/topAir/4.91/T', '', 3);
+topAirT = dlmread('./dataFiles/pgf03/postProcessing/pp0/topAir/23.85/T', '', 3);
 % topAirT = dlmread('./dataFiles/pgf04/postProcessing/pp0/topAir/7.19/T', '', 3);
 
 bottomAirT0 = 300;
-bottomAirT = dlmread('./dataFiles/pgf03/postProcessing/pp1/bottomAir/4.91/T', '', 3);
+bottomAirT = dlmread('./dataFiles/pgf03/postProcessing/pp1/bottomAir/23.85/T', '', 3);
 % bottomAirT = dlmread('./dataFiles/pgf04/postProcessing/pp1/bottomAir/7.19/T', '', 3);
 
 
@@ -42,8 +42,8 @@ bottomAirT = dlmread('./dataFiles/pgf03/postProcessing/pp1/bottomAir/4.91/T', ''
 eq1 = htc1*(topAirT0 - topAirT(:, 2))
 eq2 = htc2*(bottomAirT(:, 2) - bottomAirT0)
 
-plot(topAirT(:,1), eq1, 'linewidth', 2);
-hold on;
+% plot(topAirT(:,1), eq1, 'linewidth', 2);
+% hold on;
 % plot(bottomAirT(:,1), eq2, 'linewidth', 2);
 % legend('Top Air', 'Bottom Air');
 
